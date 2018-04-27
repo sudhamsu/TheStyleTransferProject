520f4b47583388e93541279005b27bb7cfb54102
Sudhamsu Krishna <smanne@cs.umass.edu>
2018-04-27 14:50:20 -0400

    pos tag based analysis
    (cherry picked from commit 90c10c61034a8c641d48f521949ef9de3aa6180a)
    
    # Conflicts:
    #	.idea/workspace.xml
    #	datautils.pyc

--- datautils.py
+++ datautils.py
@@ -7,7 +7,7 @@ def document_tokenize(path):
     with open(path, 'r') as f:
         text = f.read()
         text = re.sub('\n', ' ', text)
-        text = re.sub('([()[\]{}])', r' \1 ', text)
+        text = re.sub('([()[\]{}"])', r' \1 ', text)
     sentences = nltk.sent_tokenize(text)
     tokenized_sentences = []
     for s in sentences:
@@ -17,7 +17,7 @@ def document_tokenize(path):
 
 def tokenize_sent(s):
     tokens = re.split('(\w+)', s)
-    tokens = [w.strip().lower() for w in tokens if w not in ['', ' ']]
+    tokens = [w.strip().lower() for w in tokens if w.strip() != '']
     return tokens
 
 
